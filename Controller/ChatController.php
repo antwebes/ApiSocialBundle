decf8cc1f10fc11ea668f02fcc4870c076c8fcc7
Chrysweel <festixeiro@gmail.com>
2015-07-20 11:40:08 +0200

    fix error scroll in ifram chat, and join chat with params

--- Controller/ChatController.php
+++ Controller/ChatController.php
@@ -3,13 +3,17 @@
 namespace Ant\Bundle\ApiSocialBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ChatController extends Controller
 {
 
-    public function showAction()
+    public function showAction(Request $request)
     {
-        return $this->render('ApiSocialBundle:Chat:show.html.twig', array());
+    	$params = $this->getRequest()->query->all();
+    	$params_string = http_build_query($params);
+    	
+        return $this->render('ApiSocialBundle:Chat:show.html.twig', array('params' => $params_string));
     }
 
 }
