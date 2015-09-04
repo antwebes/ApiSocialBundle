97bef894011a609acc71716008dc281d17910ea1
jota_develope <jdeveloper.inxenio@gmail.com>
2015-09-04 12:56:27 +0200

    Fixed last visits template issues

--- Controller/UserProfileController.php
+++ Controller/UserProfileController.php
@@ -95,21 +95,6 @@ class UserProfileController extends BaseController
         return $response;
     }
 
-    public function renderWidgetUserVisitorsAction($user, $limit = false, $expand = false)
-    {
-        $userManager = $this->get('api_users');
-
-        if($limit){
-            $limit = $this->container->getParameter('visits_limit');
-        }else{
-            $limit = null;
-        }
-
-        $visitors = $userManager->getUserVisits($user, $limit);
-
-        return $this->render('ApiSocialBundle:User:widgetVisitors.html.twig', array('visitors' => $visitors, 'expand' => $expand));
-    }
-
     /**
      * @param $username
      * @param null $user_id
--- Controller/VisitController.php
+++ Controller/VisitController.php
@@ -4,11 +4,18 @@ namespace Ant\Bundle\ApiSocialBundle\Controller;
 
 class VisitController extends BaseController
 {
-    public function renderWidgetUserVisitorsAction($user, $limit)
+    public function renderWidgetUserVisitorsAction($user, $limit = false, $expand = false)
     {
         $userManager = $this->get('api_users');
+
+        if($limit){
+            $limit = $this->container->getParameter('visits_limit');
+        }else{
+            $limit = null;
+        }
+
         $visitors = $userManager->getUserVisits($user, $limit);
 
-        return $this->render('ApiSocialBundle:Visit:_widgetVisitors.html.twig', array('visitors' => $visitors));
+        return $this->render('ApiSocialBundle:Visit:_widgetVisitors.html.twig', array('visitors' => $visitors, 'expand' => $expand));
     }
 }
