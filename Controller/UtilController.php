d7fc5f34f03881e5a56872ccd86eae8daf30eaee
Chrysweel <festixeiro@gmail.com>
2015-10-07 16:23:31 +0200

    include second phase of adsense

--- Controller/UtilController.php
+++ Controller/UtilController.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class UtilController extends Controller {
 
-    public function generateBlockAdsenseAction($size = null)
+    public function generateBlockAdsenseAction($size = null, $styles = array('hr'=> 1))
     {
     	$twig = $this->container->get('twig');
     	$globals = $twig->getGlobals();
@@ -16,9 +16,9 @@ class UtilController extends Controller {
     	if ($globals['adsense'] && ($env !='dev')){
     		$lotery = rand(1, 100);
     		if ($lotery>60){
-    			return $this->render('ApiSocialBundle:Adsense:adsense-awc.html.twig', array('size'=> $size));
+    			return $this->render('ApiSocialBundle:Adsense:adsense-awc.html.twig', array('size'=> $size, 'styles' => $styles));
     		}else{
-    			return $this->render('ApiSocialBundle:Adsense:adsense-affiliate.html.twig', array('size'=> $size));
+    			return $this->render('ApiSocialBundle:Adsense:adsense-affiliate.html.twig', array('size'=> $size, 'styles' => $styles));
     		}
     	}
     	return new Response();
