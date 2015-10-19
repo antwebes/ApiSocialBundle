94ff9efb8cd1514066cd95d5892aace6cfb6589e
Borja <bmp_86@hotmail.com>
2015-10-19 13:41:19 +0200

    Change adsense controller to use website parameters

--- Controller/UtilController.php
+++ Controller/UtilController.php
@@ -10,10 +10,17 @@ class UtilController extends Controller {
 
     public function generateBlockAdsenseAction($size = null, $styles = array('hr'=> 1))
     {
-    	$twig = $this->container->get('twig');
-    	$globals = $twig->getGlobals();
+        
+        $parameters_service = $this->get('ant_api_social.services.parameters_service');
+        
+        if(!$parameters_service->hasParameter("container","adsense")) {
+            return new Response();
+        }
+
+        $adsense = $parameters_service->getParameter("container", "adsense");
+    	
     	$env = $this->container->getParameter("kernel.environment");
-    	if ($globals['adsense'] && ($env !='dev')){
+    	if ($adsense && ($env !='dev')){
     		$lotery = rand(1, 100);
     		if ($lotery>60){
     			return $this->render('ApiSocialBundle:Adsense:adsense-awc.html.twig', array('size'=> $size, 'styles' => $styles));
