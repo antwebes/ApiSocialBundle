a6c5899ba66b9663be92d65d0266667c17b27c7f
Chrysweel <festixeiro@gmail.com>
2015-10-28 14:30:04 +0100

    refix search param in globals of twig if not defined

--- Controller/UtilController.php
+++ Controller/UtilController.php
@@ -13,7 +13,7 @@ class UtilController extends Controller {
     	$twig = $this->container->get('twig');
     	$globals = $twig->getGlobals();
     	$env = $this->container->getParameter("kernel.environment");
-    	if ($globals['adsense'] && ($env !='dev')){
+    	if (array_key_exists('adsense', $globals) && $globals['adsense'] && ($env !='dev')){
     		$beneficiary = $this->get('ant_api_social.provider.advertising')->getBeneficiary();
     		if ($beneficiary == 'awc'){
     			return $this->render('ApiSocialBundle:Adsense:adsense-awc.html.twig', array('size'=> $size, 'styles' => $styles));
