18c561fb75666b35236fc0e2feb0992698cc7b85
jota_develope <jdeveloper.inxenio@gmail.com>
2016-01-13 11:23:09 +0100

    Added service and twig extension to check if an profile is empty

--- DependencyInjection/ApiSocialExtension.php
+++ DependencyInjection/ApiSocialExtension.php
@@ -39,6 +39,6 @@ class ApiSocialExtension extends Extension
         $container->setParameter('channels_orders', $config['channels_orders']);
         $container->setParameter('api_social.voyeur_limit', $config['voyeur_limit']);
         $container->setParameter('api_social.realtime_endpoint',$config['realtime_endpoint']);
-
+        $container->setParameter('api_social.profile_properties_to_check', $config['profile_properties_to_check']);
     }
 }
